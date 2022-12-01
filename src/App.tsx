31bc2b0a260b50152cda175d8ce0bbc35bb51d1e
Abde Ali Kanchwala <abdealikw@gmail.com>
2022-12-01 13:54:11 +0530

    adding navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Navbar from './app/components/navbar'
 import Routes from './app/routes'
 
 export const APP_TEST_ID = 'APP_TEST_ID'
@@ -6,6 +7,10 @@ export const APP_TEST_ID = 'APP_TEST_ID'
 function App() {
     return (
         <div data-testid={APP_TEST_ID} className="mx-auto">
+            <Navbar
+                brandName="Yolo Group"
+                logoUrl={'https://yolo.com/wp-content/themes/coingaming/img/coingaming.svg'}
+            />
             <Routes />
         </div>
     )
